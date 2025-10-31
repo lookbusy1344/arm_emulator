a8054104efaf06d2f1216fbadd3bf4be134dc4f1
lookbusy1344 <3680611+lookbusy1344@users.noreply.github.com>
2025-10-31 16:26:02 +0000

    GUI now accepts a filename on the command line

--- gui/main.go
+++ gui/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"embed"
+	"flag"
 	"log"
+	"os"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -13,8 +15,23 @@ import (
 var assets embed.FS
 
 func main() {
+	// Parse command line flags
+	flag.Parse()
+	
 	// Create application
 	app := NewApp()
+	
+	// Load initial file if specified
+	if flag.NArg() > 0 {
+		filePath := flag.Arg(0)
+		source, err := os.ReadFile(filePath)
+		if err != nil {
+			log.Fatalf("Failed to read file %s: %v", filePath, err)
+		}
+		if err := app.LoadProgramFromSource(string(source), filePath, 0x8000); err != nil {
+			log.Fatalf("Failed to load program: %v", err)
+		}
+	}
 
 	// Create application with options
 	err := wails.Run(&options.App{
